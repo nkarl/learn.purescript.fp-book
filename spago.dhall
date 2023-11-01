aa94f1291ba08559632ae32ebeb580944605d8a2
nkarl <21993921+nkarl@users.noreply.github.com>
2023-11-01 14:20:29 -0700

    Cleaned up.

--- spago.dhall
+++ spago.dhall
@@ -12,7 +12,12 @@ to generate this file without the comments in this block.
 -}
 { name = "my-project"
 , dependencies =
-  [ "console", "effect", "lists", "maybe", "prelude", "psci-support" ]
+  [ "console"
+  , "effect"
+  , "lists"
+  , "maybe"
+  , "prelude"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Ch5.purs
+++ src/Ch5.purs
@@ -4,9 +4,7 @@ module Ch5
   ) where
 
 import Data.List (List(..), (:))
-import Data.List.Lazy (findLastIndex)
 import Data.Maybe (Maybe(..))
-import Data.TraversableWithIndex (traverseWithIndex)
 import Effect (Effect)
 import Effect.Console (log)
 import Prelude (Unit, (+), (-), (<), (>=), (/=), (==), show, discard, negate)
@@ -40,54 +38,68 @@ singleton a = a : Nil
 -- f06. checks if a list is empty (Nil)
 nullList :: forall a. List a -> Boolean
 nullList Nil = true
+
 nullList _ = false
 
 -- f07. the snoc function that appends an element to the end of a list
 snoc :: forall a. List a -> a -> List a
 snoc Nil x = singleton x
+
 snoc (y : ys) x = y : snoc ys x
 
 -- f08. the length function calculates the length of a list
 length :: forall a. List a -> Int -> Int
 length Nil l = l
+
 length (_ : xs) l = length xs (l + 1)
 
 -- f09. the head function returns the first element of a list
 head :: forall a. List a -> Maybe a
 head Nil = Nothing
+
 head (x : _) = Just x
 
 -- f10. the tail function ignores the first element and returns the rest of the list
 tail :: forall a. List a -> Maybe (List a)
 tail Nil = Nothing
+
 tail (_ : xs) = Just xs
 
 -- f11. the last function returns the last element of a list or Nothing
 last :: forall a. List a -> Maybe a
 last Nil = Nothing
+
 last (x : Nil) = Just x
+
 last (_ : xs) = last xs
 
 -- f12. init is to last as tail is to head
 init :: forall a. List a -> Maybe (List a)
 init Nil = Nothing
+
 init l = Just $ check l
   where
   check Nil = Nil
+
   check (_ : Nil) = Nil
+
   check (x : xs) = x : check xs
 
 -- f13. uncons takes a list and return the head and the tail
 uncons :: forall a. List a -> Maybe { head :: a, tail :: List a }
 uncons Nil = Nothing
+
 uncons (x : xs) = Just { head: x, tail: xs }
 
 -- f14. index takes a list and an integer and returns the element at that position in the list
 index :: forall a. List a -> Int -> Maybe a
 index Nil _ = Nothing
+
 index _ i
   | i < 0 = Nothing
+
 index (x : _) 0 = Just x
+
 index (_ : xs) i = index xs (i - 1)
 
 infixl 8 index as !!  -- operator alias for the function index
@@ -98,18 +110,18 @@ findIndex :: forall a. (a -> Boolean) -> List a -> Maybe Int
 findIndex predicate xs = check 0 xs
   where
   check _ Nil = Nothing
-  check i (y : ys) =
-    if predicate y then
-      Just i
-    else
-      check (i + 1) ys
+
+  check i (y : ys) = if predicate y then Just i else check (i + 1) ys
 
 -- f16. takes a predicate and a list and return the last element that matches that predicate
 findLastIndex :: forall a. (a -> Boolean) -> List a -> Maybe Int
 findLastIndex _ Nil = Nothing
-findLastIndex predicate xs = check 0 xs
+
+findLastIndex predicate xs = check Nothing 0 xs
   where
-    check i (y : ys)
+  check fi _ Nil = fi
+
+  check fi i (y : ys) = check (if predicate y then Just i else fi) (i + 1) ys
 
 -- the test function
 test :: Effect Unit
