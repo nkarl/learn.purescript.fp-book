6951902e69ca4ac71e53e75e18d63433e98bae41
nkarl <21993921+nkarl@users.noreply.github.com>
2024-03-20 15:31:40 -0700

    Updated. Continued studying the fp made easier book. Studying the Reader monad.

--- src/Reader/Monadic01.purs
+++ src/Reader/Monadic01.purs
@@ -10,31 +10,38 @@ newtype Reader r a
 instance functorReader :: Functor (Reader r) where
   map f (Reader g) = Reader \r -> f $ g r
 
+--cf    :: Reader r (a -> b)
+--cf r  :: (a -> b)
+--cx    :: Reader r  a
+--cx r  :: a
 instance applyReader :: Apply (Reader r) where
-  apply (Reader ff) (Reader fx) = Reader \r -> ff r $ fx r
+  --apply :: forall a b. Reader r (a -> b) -> Reader r a -> Reader r b
+  apply (Reader cf) (Reader cx) = Reader \r -> cf r $ cx r
+  --apply (Reader cf) (Reader cx) = Reader \r -> (apply cf cx) r
 
 instance applicativeReader :: Applicative (Reader r) where
   --pure x = Reader \_ -> x
   pure = Reader <<< const
 
 --                      f           ::       a -> Reader r b
---                          fx      :: (r -> a)
---                          fx r    ::       a
---                      f $ fx r    ::            Reader r b
--- Reader \r ->         f $ fx r    :: Reader r  (Reader r b) -- join but we want something leaner
+--                          cx      :: (r -> a)
+--                          cx r    ::       a
+--                      f $ cx r    ::            Reader r b
+-- Reader \r ->         f $ cx r    :: Reader r  (Reader r b) -- join but we want something leaner
 --                                     ^
 --                                  this is where we can use `join`
 --                                  however we desire something leaner
---------------- unwrap (f $ fx r)   ::                r -> b
---------------- unwrap (f $ fx r) r ::                     b
--- Reader \r -> unwrap (f $ fx r) r :: Reader r            b
+--------------- unwrap (f $ cx r)   ::                r -> b
+--------------- unwrap (f $ cx r) r ::                     b
+-- Reader \r -> unwrap (f $ cx r) r :: Reader r            b
 instance bindReader :: Bind (Reader r) where
-  --bind (Reader fx) f = join $ Reader \r -> f $ fx r
-  --bind (Reader fx) f = Reader \r -> runReader (f $ fx r) r
-  bind (Reader fx) f = Reader \r -> (unwrap $ transform r) r
+  --bind :: forall a b. Reader r a -> (a -> Reader r b) -> Reader r b
+  --bind (Reader cx) f = join $ Reader \r -> f $ cx r
+  --bind (Reader cx) f = Reader \r -> runReader (f $ cx r) r
+  bind (Reader cx) f = Reader \r -> (unwrap $ transform r) r
     where
       unwrap (Reader c) = c
-      transform         = \r -> f $ fx r
+      transform         = \r -> f $ cx r
 
 runReader :: forall a r. Reader r a -> r -> a
 runReader (Reader f) = f
@@ -44,3 +51,4 @@ instance monadReader :: Monad (Reader r)
 test :: Effect Unit
 test = do
   log $ show $ "placeholder"
+
