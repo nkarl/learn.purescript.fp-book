1ec3bb6184f058eab99fc5c8b549f536cf2dddd8
nkarl <21993921+nkarl@users.noreply.github.com>
2024-04-07 09:39:00 -0700

    Solved the type problem with take1char', was a problem of too general a type e in signature. Solved by constraining to the specific type Error. Next, see if I can typeclass this function.

--- src/Parser/Monadic02.purs
+++ src/Parser/Monadic02.purs
@@ -10,76 +10,90 @@ import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Class.Console (log)
 
--- NOTE: input: a string such as "ABC"
-type State a
-  = Tuple String a
+{-
+  NOTE: input: a string such as "ABC"
+-}
+type {-- alias --} State a = Tuple String a
 
-type Action' e a
-  = Failure e => String -> Either e (State a)
+-- The polymorphic type `e` (the first param of Either) is constrained to those that have an instance of typeclass Failing.
+type {-- alias --} Action e a = String -> Failing e => Either e (State a)
 
-newtype Action e a
-  = Action (Action' e a)
+newtype Process e a = Process (Action e a)
 
-class Failure (e :: Type) where
-  eof :: e
+-- Error
+data Error = EOF
 
--- eof Error
-data ErrEOF
-  = EOF
+derive instance Generic Error _
 
-derive instance genericErrEOF :: Generic ErrEOF _
-
-instance showErrEOF :: Show ErrEOF where
+instance Show Error where
   show = genericShow
 
-instance parseErrEOF :: Failure ErrEOF where
+-- the polymorphic type `e` is a Type and not a Kind
+class Failing (e :: Type) where
+  eof :: e
+
+instance Failing Error where
   eof = EOF
 
--- NOTE: functor instances
+{-
+  NOTE: functor instances
+-}
 -- functor
-instance functorAction :: Functor (Action e) where
-  map f (Action ax) = Action \s -> map f <$> ax s
+instance Functor (Process e) where
+  map f (Process px) = Process \s -> map f <$> px s
 
 -- apply
-instance applyAction :: Apply (Action e) where
-  apply (Action af) (Action ax) =
-    Action \s -> do
-      Tuple s' f <- af s
-      map f <$> ax s'
+instance Apply (Process e) where
+  apply (Process pf) (Process px) =
+    Process \s -> do
+      Tuple s' f <- pf s
+      map f <$> px s'
 
 -- applicative
-instance applicativeAction :: Applicative (Action e) where
-  pure x = Action \s -> pure $ Tuple s x
+instance Applicative (Process e) where
+  pure x = Process \s -> pure $ Tuple s x
 
 -- bind
-instance bindAction :: Bind (Action e) where
-  bind (Action ax) f =
-    Action \s -> do
-      Tuple s' x <- ax s
-      f x # \(Action f') -> f' s'
+instance Bind (Process e) where
+  bind (Process px) f =
+    Process \s -> do
+      Tuple s' x <- px s
+      f x # \(Process f') -> f' s'
 
 -- monad
-instance monadAction :: Monad (Action e)
+instance Monad (Process e)
 
--- NOTE: data: Tuple of remaining string and a string of taken chars
-take1char' :: forall e. (Action e) Char
-take1char' =
-  Action \s -> case uncons s of
-    Nothing -> Left eof
-    Just { head, tail } -> Right $ Tuple tail head
+{-
+  NOTE: data: Tuple of remaining string and a string of taken chars
+-}
+-- Process e Char :: String -> Failing e => Either e (Tuple String Char)
+class TakeOneChar e where
+  take1char :: forall e. Process e Char
 
--- NOTE: take some chars from the string
-take2chars :: forall e. (Action e) String
-take2chars = do
-  c1 <- take1char'
-  c2 <- take1char'
-  pure $ fromCharArray [ c1, c2 ]
+instance TakeOneChar Error where
+  take1char =
+    Process \s -> do
+      let
+          x = eof
+          --y = EOF
+      case uncons s of
+        Nothing -> Left x-- y :: (Either Error _)
+        Just { head, tail } -> Right $ Tuple tail head
+
+{-
+  NOTE: take some chars from the string
+-}
+--take2chars :: (Process Error) String
+--take2chars = do
+  --c1 <- take1char
+  --c2 <- take1char
+  --pure $ fromCharArray [ c1, c2 ]
 
 {-- helper --}
-unwrap :: forall e a. (Action e) a -> (Action' e) a
-unwrap (Action f) = f
+unwrap :: forall e a. (Process e) a -> Action e a
+unwrap (Process f) = f
 
-unwrap' :: forall a. (Action ErrEOF) a -> (Action' ErrEOF) a
+unwrap' :: forall a. (Process Error) a -> (Action Error) a
 unwrap' = unwrap
 
 -- NOTE: unit test
@@ -89,8 +103,8 @@ test = do
     $ show do
         let
           x = "ABC"
-          y = (unwrap' take1char') $ x
-        (y)
-  log $ show $ (unwrap' take2chars $ "ABC")
-  log $ show $ (unwrap' take2chars $ "AB")
-  log $ show $ (unwrap' take2chars $ "A")
+          y = take1char $ x -- :: Either Error _
+        y
+  --log $ show $ (unwrap' take2chars $ "ABC")
+  --log $ show $ (unwrap' take2chars $ "ABC")
+  --log $ show $ (unwrap' take2chars $ "AB")
