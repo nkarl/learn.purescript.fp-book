fc87591f80b04f3b0f852d9387886692ed25531d
nkarl <21993921+nkarl@users.noreply.github.com>
2024-04-07 12:31:48 -0700

    Flipped the name of Action and Process for Parser.Monadic02, because it makes more sense that way.

--- src/Parser/Monadic02.purs
+++ src/Parser/Monadic02.purs
@@ -5,7 +5,7 @@ import Data.Either (Either(..))
 import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe(..))
 import Data.Show.Generic (genericShow)
-import Data.String.CodeUnits (fromCharArray, uncons)
+import Data.String.CodeUnits (uncons)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Class.Console (log)
@@ -16,9 +16,9 @@ import Effect.Class.Console (log)
 type {-- alias --} State a = Tuple String a
 
 -- The polymorphic type `e` (the first param of Either) is constrained to those that have an instance of typeclass Failing.
-type {-- alias --} Action e a = String -> Failing e => Either e (State a)
+type {-- alias --} Process e a = String -> Failing e => Either e (State a)
 
-newtype Process e a = Process (Action e a)
+newtype Action e a = Action (Process e a)
 
 -- Error
 data Error = EOF
@@ -39,61 +39,57 @@ instance Failing Error where
   NOTE: functor instances
 -}
 -- functor
-instance Functor (Process e) where
-  map f (Process px) = Process \s -> map f <$> px s
+instance Functor (Action e) where
+  map f (Action px) = Action \s -> map f <$> px s
 
 -- apply
-instance Apply (Process e) where
-  apply (Process pf) (Process px) =
-    Process \s -> do
+instance Apply (Action e) where
+  apply (Action pf) (Action px) =
+    Action \s -> do
       Tuple s' f <- pf s
       map f <$> px s'
 
 -- applicative
-instance Applicative (Process e) where
-  pure x = Process \s -> pure $ Tuple s x
+instance Applicative (Action e) where
+  pure x = Action \s -> pure $ Tuple s x
 
 -- bind
-instance Bind (Process e) where
-  bind (Process px) f =
-    Process \s -> do
+instance Bind (Action e) where
+  bind (Action px) f =
+    Action \s -> do
       Tuple s' x <- px s
-      f x # \(Process f') -> f' s'
+      f x # \(Action f') -> f' s'
 
 -- monad
-instance Monad (Process e)
+instance Monad (Action e)
 
 {-
   NOTE: data: Tuple of remaining string and a string of taken chars
 -}
--- Process e Char :: String -> Failing e => Either e (Tuple String Char)
-class TakeOneChar e where
-  take1char :: forall e. Process e Char
-
-instance TakeOneChar Error where
-  take1char =
-    Process \s -> do
-      let
-          x = eof
-          --y = EOF
+-- Action e Char :: String -> Failing e => Either e (Tuple String Char)
+--class TakeOneChar e where
+take1char :: forall e. Action e Char
+--instance TakeOneChar Error where
+take1char =
+    Action \s ->
       case uncons s of
-        Nothing -> Left x-- y :: (Either Error _)
+        Nothing -> Left eof -- :: (Either Error _)
         Just { head, tail } -> Right $ Tuple tail head
 
 {-
   NOTE: take some chars from the string
 -}
---take2chars :: (Process Error) String
+--take2chars :: (Action Error) String
 --take2chars = do
   --c1 <- take1char
   --c2 <- take1char
   --pure $ fromCharArray [ c1, c2 ]
 
 {-- helper --}
-unwrap :: forall e a. (Process e) a -> Action e a
-unwrap (Process f) = f
+unwrap :: forall e a. (Action e) a -> Process e a
+unwrap (Action f) = f
 
-unwrap' :: forall a. (Process Error) a -> (Action Error) a
+unwrap' :: forall a. (Action Error) a -> (Process Error) a
 unwrap' = unwrap
 
 -- NOTE: unit test
@@ -103,7 +99,7 @@ test = do
     $ show do
         let
           x = "ABC"
-          y = take1char $ x -- :: Either Error _
+          y = unwrap' take1char $ x -- :: Either Error _
         y
   --log $ show $ (unwrap' take2chars $ "ABC")
   --log $ show $ (unwrap' take2chars $ "ABC")
