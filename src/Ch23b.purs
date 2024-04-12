cd260d24b054613c00e9a4b50021af3f2541182c
nkarl <21993921+nkarl@users.noreply.github.com>
2024-04-11 20:55:11 -0700

    Reviewed 23b.purs/RNG. Reviewed the signature of runFiberM, ie how to run a fiber in an Aff.

--- src/Ch23b.purs
+++ src/Ch23b.purs
@@ -1,7 +1,6 @@
 module Ch23b where
 
 import Prelude
-
 import Control.Monad.Reader.Trans (ReaderT, runReaderT)
 import Control.Monad.State.Trans (StateT, runStateT)
 import Data.Either (Either(..))
@@ -16,36 +15,37 @@ import Effect.Random (random)
 {--
   NOTE: DATA-TYPES and TYPE-ALIASES
 --}
-type StrBus = BusRW String -- `BusRW :: Bus ( read :: Cap, write :: Cap)` is a row type. It reads and writes strings.
+type StrBus
+  = BusRW String -- `BusRW :: Bus ( read :: Cap, write :: Cap)` is a row type. It reads and writes strings.
 
-type Config {-- product type --}
+type Config  {-- product type --}
   = { bus :: StrBus } -- NOTE: could contain more properties, for example env variables.
 
-type State {-- product type --}
+type State  {-- product type --}
   = { count :: Int }
 
 --Callback  :: (Either Error a -> Effect Unit)
 --makeAff   ::  Callback a -> Effect Canceler -> Aff a
-
 {--
   NOTE:
     - each fiber is composed as a stack of Reader $ State $ Aff
     - a note on type definitions.
 --}
 --   FiberM a = ReaderT r       m                 a
-type FiberM a = ReaderT Config (StateT State Aff) a
+type FiberM a
+  = ReaderT Config (StateT State Aff) a
 
 {--
   NOTE: CONSTRUCTS
 --}
-
 {--
   NOTE: how to run something inside a fiber?
     - we look at the definition of `FiberM`. "Running" means that given an input `FiberM` we produce some output wrapped in an `Aff`.
 --}
 runFiberM :: Config -> FiberM Unit -> Aff (Tuple Unit State)
-runFiberM config = flip runStateT {count: 10}  -- flip so that we take the `StateT st` later
-                    <<< flip runReaderT config  -- flip so that we take `ReaderT rt` later
+runFiberM config =
+  flip runStateT { count: 10 } -- flip so that we pass `StateT  sT` later
+    <<< flip runReaderT config -- flip so that we pass `ReaderT rT` later
 
 randomAff :: Aff Number
 randomAff =
@@ -67,4 +67,3 @@ test = do
     forkAff lets us fork another fiber inside an Aff.
     launchAff launches a fiber inside an Effect. In other words, launchAff creates an patient-zero fiber.
 --}
-
