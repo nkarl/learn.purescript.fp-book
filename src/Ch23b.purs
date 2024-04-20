02d748a343213173da3a0c7d6653581904daf2bb
nkarl <21993921+nkarl@users.noreply.github.com>
2024-04-19 21:31:33 -0700

    Working on RNG re 01.

--- src/Ch23b.purs
+++ src/Ch23b.purs
@@ -26,23 +26,24 @@ type Reader = { bus :: StrBus }
 type State  = { count :: Int }
 {--
   NOTE: can it be replaced with `forE` instead?
-    - `forE` would NOT work, because `count` is modified on each fiber spawning. Leaving `count` in a globally accessible state allows non-linear growth of fibers.
+    - `forE` would NOT work, because `count` is modified on each fiber spawning.
+        - Leaving `count` in a global state allows for non-linear growth of fibers.
     - `forE`restricts fiber spawning to linear growth; only one fiber can be spawn at a time.
         - This is fine if there is only one predicate. However,
         - we need to spawn 3 fibers with indenpendent predicates concurrently.
-        - in other words, `count` needs to float so that fibers can spawn; it cannot be a linear iterator a la `forE` in this case.
+    - in other words, `count` needs to "float" so that fibers can spawn; it cannot be a linear iterator a la `forE` in this case.
 --}
 
 -- each fiber is composed as a stack of Reader $ State $ Aff
---  FiberM a :: ReaderT r      m                  a
-type FiberM a = ReaderT Reader (StateT State Aff) a
+--  FiberMS a :: ReaderT r      m                    a
+type FiberMS a = ReaderT Reader (StateT State (Aff)) a
 
 {--
   NOTE: ACTIONS
 --}
 
---Cb      :: (Either Error a -> Effect Unit)
---makeAff ::  Cb a -> Effect Canceler -> Aff a
+--Callback :: (Either Error a -> Effect Unit)
+--makeAff  :: Callback a -> Effect Canceler -> Aff a
 -- | a random number lifted into Aff.
 affRandom :: Aff Number
 affRandom =
@@ -54,30 +55,30 @@ affRandom =
 
 {--
   - how to run something inside a fiber?
-    - "running" means that given an input `FiberM`, we produce some output wrapped in an `Aff`.
-    - we look at the definition of `FiberM`, ie our monad stack.
+    - "running" means that given an input `FiberMS`, we produce some output wrapped in an `Aff`.
+    - we look at the definition of `FiberMS`, ie our monad stack.
       - ReaderT is outermost, then StateT and finally Aff a.
       - we don't need anything return from Aff, so we return a `Unit` wrapped by Aff.
 --}
 -- | takes a BusRW and returns a fibric function that will process later.
-runFiberM :: BusRW String -> (FiberM Unit -> Aff Unit) -- (Tuple Unit State)
+runFiberM :: BusRW String -> (FiberMS Unit -> Aff Unit) -- (Tuple Unit State)
 runFiberM bus =
   void -- coerce the polymorphic type `a` to Unit
     <<< forkAff -- fork a new fiber for this call
-    -- runStateT  :: StateT  s m a -> s -> m a
+    -- runStateT  :: (StateT  s m a) -> s -> m a
     <<< flip runStateT  { count: 10 } -- flip to take `s` first and `StateT s m a` later, partial
-    -- runReaderT :: ReaderT r m a -> r -> m a
+    -- runReaderT :: (ReaderT r m a) -> r -> m a
     <<< flip runReaderT { bus }       -- flip to take `r` first and `ReaderT r m a` later, partial
 
 -- | subscribe fiber
-logger :: FiberM Unit
+logger :: FiberMS Unit
 logger = forever do
   { bus } <- ask
   s       <- liftAfftoFiberM $ Bus.read bus
   log $ "Logger: " <> s
 
 -- | factored out
-liftAfftoFiberM :: Aff ~> FiberM
+liftAfftoFiberM :: Aff ~> FiberMS
 liftAfftoFiberM = lift <<< lift
 
 -- | the desired randomized value wrapped in Aff.
@@ -85,16 +86,15 @@ delayRandom :: Aff Number
 delayRandom = delay (Milliseconds 1000.0) *> affRandom
 
 -- | publish/broadcast fiber
-randomGenerator :: String -> (Number -> Boolean) -> FiberM Unit
+randomGenerator :: String -> (Number -> Boolean) -> FiberMS Unit
 randomGenerator predLabel pred = do
   { count } <- get
   unless (count <= 0) do
      { bus } <- ask
      liftAfftoFiberM do
         n <- delayRandom
-        let
-            output = "Found a value that is " <> predLabel <> " (" <> show n <> ")"
-        when (pred n) $ flip Bus.write bus $ output
+        let output = "Found a value that is " <> predLabel <> " (" <> show n <> ")"
+        when (pred n) $ Bus.write output bus
      --put { count: count - 1 }
      modify_ _ {count = count - 1}
      randomGenerator predLabel pred
@@ -105,11 +105,9 @@ test = launchAff_ do
   bus <- Bus.make
   let forkFiberM = runFiberM bus
   forkFiberM $ logger
-  forkFiberM $ randomGenerator "greater than 0.5" (_ > 0.5)
-  forkFiberM $ randomGenerator "less than 0.5"    (_ < 0.5)
-  forkFiberM $ randomGenerator "greater than 0.1" (_ > 0.1)
-  -- randomGenerator
-  pure unit
+  forkFiberM $ randomGenerator "greater than 0.5\t" (_ > 0.5)
+  forkFiberM $ randomGenerator "less    than 0.5\t" (_ < 0.5)
+  forkFiberM $ randomGenerator "greater than 0.1\t" (_ > 0.1)
 
 {--
   NOTE: on `launchAff` and `forkAff`
--- src/RNG/RandomNumber01.purs
+++ src/RNG/RandomNumber01.purs
@@ -2,8 +2,65 @@ module RNG.RandomNumber01 where
 
 import Prelude
 
+import Control.Monad.Cont (lift)
+import Control.Monad.Reader.Trans (ReaderT, runReaderT)
+import Control.Monad.State.Trans (StateT, runStateT)
+import Data.Foldable (and)
+import Data.Tuple (Tuple)
 import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Aff.Bus (BusRW)
+import Effect.Class (liftEffect)
+import Effect.Class as Effect.Class
 import Effect.Class.Console (log)
+import Effect.Random (random)
+
+{-
+  PROGRAM SPECS:
+    - 3 broadcasters
+      - one checking if a random number is: larger than 0.5 ( > 0.5)
+      - one checking if a random number is: less   than 0.5 ( < 0.5)
+      - one checking if a random number is: larger than 0.1 ( > 0.1)
+    - 1 subscriber
+      - reads the global bus and logs whatever string in the bus.
+    - we use Aff to accomodate the 4 fibers of pubs and sub.
+      - Thus Aff is the working base (async portion of the program), just above Effect.
+        - NOTE:
+          - every program starts at `Effect` as absolute base.
+            - when concurrency is needed, we use `Aff` inside Effect. Aff let us manage multiple `Fiber`, which can be forked from Aff.
+  PROGRAM CONSTRUCTION:
+    1. [x] generate a random value wrapped in an Aff.
+    2. [x] create a monad stack, and
+      2.1. [ ] run that monad stack in a fiber.
+    3. [ ] create a Bus.
+    4. [ ] publish to a Bus.
+    5. [ ] subscribe to a Bus.
+-}
+
+{-- TYPES & ALIASES --}
+
+type Bus      = BusRW String
+
+type Reader   = { bus :: Bus }
+
+type State    = { count :: Int }
+
+-- Aff Unit at the core
+
+--   FiberM a :: trans  r       m                  a
+type FiberMS a = ReaderT Reader (StateT State Aff) a
+
+{-- FUNCTIONS --}
+
+-- | creates a random value wrapped in Aff
+affRandom :: Aff Number
+affRandom = Effect.Class.liftEffect random -- lift types `Number` from Effect to Aff.
+
+runFiberM :: BusRW String -> (FiberMS Unit -> Aff Unit)-- (Tuple Unit State))
+runFiberM bus =
+  void
+  <<< flip runStateT {count: 10}
+  <<< flip runReaderT { bus }
 
 test :: Effect Unit
 test = do
