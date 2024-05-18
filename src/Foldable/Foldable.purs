b13dd3b270a7ab3ed633f40852d11fe4b28483d4
nkarl <21993921+nkarl@users.noreply.github.com>
2024-05-17 20:20:05 -0700

    Updated. Working on Traversable as extension of Foldable.

--- src/Foldable/Foldable.purs
+++ src/Foldable/Foldable.purs
@@ -3,35 +3,37 @@ module Foldable.Foldable where
 import Prelude
 import Data.Generic.Rep (class Generic)
 import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
 import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Class.Console (log)
 
 class Foldable f where
-  -- -- foldr (+) acc [1, 2, 3] = 1 + (2 + (3 + acc))
-  foldr :: forall a b. (a -> b -> b) -> b -> f a -> b
-  -- -- foldl (+) acc [1, 2, 3] = ((acc + 1) + 2) + 3
-  foldl :: forall a b. (b -> a -> b) -> b -> f a -> b
-  -- -- map a function `f` onto a Foldable `ls` producing a Monoid `m`
-  foldM :: forall a m. Monoid m => (a -> m) -> f a -> m
+  foldr :: forall a b. (a -> b -> b) -> b -> f a -> b   -- foldr (+) acc [1, 2, 3] = 1 + (2 + (3 + acc))
+  foldl :: forall a b. (b -> a -> b) -> b -> f a -> b   -- foldl (+) acc [1, 2, 3] = ((acc + 1) + 2) + 3
+  foldM :: forall a m. Monoid m => (a -> m) -> f a -> m -- map `f` onto a Foldable `ls` accumulating in a Monoid `m`
 
 instance foldableList :: Foldable List where
-  foldr _ acc Nil = acc
-  foldr f acc (x : xs) = x `f` (foldr f acc xs)
-  foldl _ acc Nil = acc
-  foldl f acc (x : xs) = foldl f acc' xs
+  foldr _ acc Nil       = acc
+  foldr f acc (x : xs)  = x `f` acc'
+    where
+    acc' = foldr f acc xs
+  foldl _ acc Nil       = acc
+  foldl f acc (x : xs)  = foldl f acc' xs
     where
     acc' = acc `f` x
   foldM _ Nil = mempty -- enum Empty
-  foldM f ls = foldl (\acc x -> acc <> f x) mempty ls
+  foldM f ls = foldl g mempty ls
+    where
+    g acc = \x -> acc <> f x
 
 data Result
   = Result
-    { list :: List Int
-    , acc :: Int
-    , op :: String
-    , fold :: String
-    , result :: Int
+    { list    :: List Int
+    , acc     :: Int
+    , op      :: String
+    , fold    :: String
+    , result  :: Maybe Int
     }
 
 derive instance genericResult :: Generic Result _
@@ -42,8 +44,23 @@ instance showResult :: Show Result where
 test :: Effect Unit
 test = do
   let
-    ls = (10 : 20 : 30 : Nil)
-
-    acc = 1
-  log $ show $ Result { list: ls, acc: acc, op: "(+)", fold: "L", result: foldl (+) acc ls }
-  log $ show $ Result { list: ls, acc: acc, op: "(+)", fold: "R", result: foldr (+) acc ls }
+    list = (10 : 20 : 30 : Nil)
+    start = 1
+  log $ show "Header: foldl"
+  log $ show
+    $ Result
+        { list  : list
+        , op    : "(+)"
+        , fold  : "L"
+        , acc   : start
+        , result: Just $ foldl (+) start list
+        }
+  log $ show "Header: foldr"
+  log $ show
+    $ Result
+        { list  : list
+        , op    : "(+)"
+        , fold  : "R"
+        , acc   : start
+        , result: Just $ foldr (+) start list
+        }
