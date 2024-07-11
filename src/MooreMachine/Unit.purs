ac29830c3db75aaf4b5665d21e5dd338db4b55bd
nkarl <21993921+nkarl@users.noreply.github.com>
2024-07-11 11:40:01 -0700

    Working on the MooreMachine (FSM).

--- src/MooreMachine/Unit.purs
+++ src/MooreMachine/Unit.purs
@@ -9,37 +9,44 @@ import Effect.Class.Console (log)
 
 import Data.Profunctor (class Profunctor)
 
-print :: forall a (m ∷ Type -> Type). MonadEffect m => Show a => a -> m Unit
-print = log <<< show
+type ExtractG s c = s -> c
+type StepT    s a = s -> a -> s
 
-type Output s b = s -> b
-
-type Step   s a = s -> a -> s
-
-data FSM s a b
-  = FSM s (s -> b) (s -> a -> s)
+--data FSM s a c
+-- = FSM s (s -> c) (s -> a -> s)
+data FSM s a c
+  = FSM s (ExtractG s c) (StepT s a)
 
 instance profunctorFSM :: Profunctor (FSM s) where
-  dimap :: forall a b c d. (c -> a) -> (b -> d) -> FSM s a b -> FSM s c d
-  dimap f g (FSM s0 output step) = FSM s0 (g <<< output) (\s -> step s <<< f)
--- FSM s a b
--- FSM s c d
+  dimap :: forall a b c d. (a -> b) -> (c -> d) -> FSM s b c -> FSM s a d
+  dimap f g (FSM s0 extractG stepT) = FSM s0 (g <<< extractG) (\s -> stepT s <<< f)
 
--- g                :: b -> d
--- output           :: s -> b
--- g . output       :: s -> d
+{--
+  NOTE: in mapping between the two states: from (FSM s b c) 
+                                           to   (FSM s a d),
+  we have 2 variant actions:
+    - contra-variant: b -> a
+    - co-variant    : c -> d
+--}
 
--- f                :: c -> a
--- step             :: s -> a -> s
--- step s           :: a -> s
--- step s . f       :: c -> s
--- \s -> step s . f :: s -> (c -> s)
+-- FSM s b c :: FSM s (s -> c) (s -> b -> s)
+-- FSM s a d :: FSM s (s -> d) (s -> a -> s)
+
+-- extractFn          :: s -> c -- Extract
+-- g                  :: c -> d
+-- g . extractFn      :: s -> d
+
+-- f                  :: a -> b
+-- stepFn             :: s -> b -> s -- Step
+-- stepFn s           :: b -> s
+-- stepFn s . f       :: a -> s
+-- \s -> stepFn s . f :: s -> (a -> s)
 
 -- state S
 data Oven = Off | Bake | Idling
 -- input \Sigma
 data Signal = ButtonBake | ButtonOff | TooHot | TooCold 
--- output \Lambda
+-- extractFn \Lambda
 data Heat = Hot | Cold
 
 -- checks a subset of the Cartesian product of S and \Sigma 
@@ -54,11 +61,24 @@ stepFn x y = go $ Tuple x y
       Tuple Idling  ButtonOff   -> Off
       Tuple s       _           -> s
 
-outputFn :: Oven -> Heat
-outputFn = case _ of
+extractFn :: Oven -> Heat
+extractFn = case _ of
   Off     -> Cold
   Bake    -> Hot
   Idling  -> Cold
+
+{--
+  TODO:
+    1. [ ] review the FSM and profunctor
+    2. [ ] aim for a _working_ understanding
+    3. [ ] write unit tests
+      - basic unit tests
+      - comparative unit tests with `foldL`
+    4. [ ] review and bolster my understanding in the context of the unit tests
+--}
+
+print :: forall a (m ∷ Type -> Type). MonadEffect m => Show a => a -> m Unit
+print = log <<< show
 
 test :: Effect Unit
 test = do
